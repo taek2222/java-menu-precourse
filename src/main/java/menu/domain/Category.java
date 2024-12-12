677567b92e9df2a972b826d4b235b2fd6e8487e0
taek2222 <oot309@daum.net>
2024-12-12 12:57:02 +0900

    feat: 카테고리 무작위 선정 기능 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,5 +1,8 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+
 public enum Category {
     JAPANESE("일식"),
     KOREAN("한식"),
@@ -13,5 +16,13 @@ public enum Category {
 
     Category(final String name) {
         this.name = name;
+    }
+
+    public static Category getRandomCategory() {
+        int number = Randoms.pickNumberInRange(1, 5);
+        return Arrays.stream(values())
+                .filter(category -> category.ordinal() == number)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("서버 에러 발생 : 카테고리 코드가 잘못 설정되어 있습니다."));
     }
 }
