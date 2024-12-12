3e13c242f11e04cfcf16f9eed4f648b7ce99b68a
taek2222 <oot309@daum.net>
2024-12-12 13:15:06 +0900

    feat: 코치 개인 무작위 음식 추천 기능 추가
    - 한 주에 중복되거나 못먹는 음식은 포함되지 않도록 했습니다.

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,17 +1,30 @@
 package menu.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
 
     private final String name;
     private final List<Food> notEats; // 없는 경우 None 처리 중임
+    private final List<Food> eats;
 
     public Coach(final String name, final List<Food> notEats) {
         validateName(name);
         validateNotEats(notEats);
         this.name = name;
         this.notEats = notEats;
+        this.eats = new ArrayList<>();
+    }
+
+    private void generateEatsIfCategory(Category category) {
+        while (true) {
+            Food food = Food.getRandomFoodIfCategory(category);
+            if (!notEats.contains(food) && !eats.contains(food)) {
+                eats.add(food);
+                break;
+            }
+        }
     }
 
     private void validateNotEats(List<Food> eats) {
