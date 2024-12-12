dece8de2b00f5d7609d517d279e5ca4bd42a8483
taek2222 <oot309@daum.net>
2024-12-12 11:47:57 +0900

    feat: 점심 메뉴 시작 메시지 출력

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,12 @@
+package menu.view;
+
+public class OutputView {
+
+    public void printMenuStartMessage() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+}
